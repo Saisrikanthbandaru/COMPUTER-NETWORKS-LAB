9a3e2565f890215f2b3e47efe1c276b25b8a8503
Saisrikanthbandaru <118458086+Saisrikanthbandaru@users.noreply.github.com>
2023-08-24 08:29:45 +0530

    Add files via upload

---
+++ bit stuffing c program..slo c.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	char data[20],stuffedData[30];
+	int i,j,count;
+	printf("enter the data");
+	scanf("%s",data);
+	//initialize variables
+	i=0;
+	j=0;
+	count=0;
+	//perform bit stuffing
+	while (data[i] !='\0'){
+	stuffedData[j]=data[i];
+	if(data[i]=='1'){
+	count++;
+}
+else{
+	count=0;
+}
+if(count==5){
+stuffedData[++j]='0';
+count=0;
+}
+i++;
+j++;
+}
+stuffedData[j]='\0';
+printf("Original Data: %s \n",data);
+printf("After Bit stuffing: %s \n",stuffedData);
+return 0;
+}
+
+
